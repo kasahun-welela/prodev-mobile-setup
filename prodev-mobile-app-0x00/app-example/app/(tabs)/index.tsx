de4f23034332fa827f9e37a33a9049ac1db42e1d
kasahun-welela <kasahunwelela1@gmail.com>
2025-07-13 12:32:37 +0300

    modify the home screen

--- prodev-mobile-app-0x00/app-example/app/(tabs)/index.tsx
+++ prodev-mobile-app-0x00/app-example/app/(tabs)/index.tsx
@@ -18,7 +18,7 @@ export default function HomeScreen() {
       }
     >
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome! back Kasahun</ThemedText>
+        <ThemedText type="title">First App Created</ThemedText>
         <HelloWave />
         <Text>Hello</Text>
       </ThemedView>
