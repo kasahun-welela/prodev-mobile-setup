e29617dc7d2b8eaeb843dd2b7c79e4f51f8660da
kasahun-welela <kasahunwelela1@gmail.com>
2025-07-18 16:35:31 +0300

    add extra three text field

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -11,6 +11,13 @@ export default function Index() {
         React Native provides you a single codebase for cross platforms
       </Text>
       <Text style={styles.smallText}>ALX is awesome</Text>
+      <Text style={styles.largeText}>
+        Typescript is great if you practice more
+      </Text>
+      <Text style={styles.mediumText}>
+        React Native provides you a single codebase for cross platforms
+      </Text>
+      <Text style={styles.smallText}>ALX is awesome</Text>
     </View>
   );
 }
